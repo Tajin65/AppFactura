12d1cadafc0ac7260e4116a64d2fc2432728ff50
Jorge Alberto Chí León <46463280+JorchCanelo@users.noreply.github.com>
2026-03-29 22:26:51 -0600

    Delete src directory

--- src/components/ui/input.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Input({ className = "", ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={`w-full rounded border border-slate-300 px-2 py-1 ${className}`.trim()} {...props} />;
-}
--- src/components/ui/table.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Table = (props: React.TableHTMLAttributes<HTMLTableElement>) => <table {...props} />;
-export const TableHeader = (props: React.HTMLAttributes<HTMLTableSectionElement>) => <thead {...props} />;
-export const TableBody = (props: React.HTMLAttributes<HTMLTableSectionElement>) => <tbody {...props} />;
-export const TableRow = (props: React.HTMLAttributes<HTMLTableRowElement>) => <tr {...props} />;
-export const TableHead = (props: React.ThHTMLAttributes<HTMLTableCellElement>) => <th {...props} />;
-export const TableCell = (props: React.TdHTMLAttributes<HTMLTableCellElement>) => <td {...props} />;
